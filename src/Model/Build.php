03c603082f8817040f03e9fece2d152cb70c6a62
Dmitry Khomutov <poisoncorpsee@gmail.com>
2018-04-25 18:53:00 +0700

    More tests.

--- src/Model/Build.php
+++ src/Model/Build.php
@@ -302,7 +302,7 @@ class Build extends BaseBuild
                 continue;
             }
 
-            $className = '\PHPCensor\Plugin\\'.$item->getBasename('.php');
+            $className = '\PHPCensor\Plugin\\' . $item->getBasename('.php');
 
             $reflectedPlugin = new \ReflectionClass($className);
 
